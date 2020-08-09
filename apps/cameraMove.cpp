b68dfe4b77892936cd57274d76120e8129a0f689
Alexander Shamanskiy <shamanskiy.alexander@gmail.com>
2020-08-09 21:25:20 +0200

    first cmake version works on Ubuntu

--- apps/cameraMove.cpp
+++ apps/cameraMove.cpp
@@ -21,9 +21,9 @@ const GLint WIDTH = 800, HEIGHT = 600;
 std::vector<Mesh *> objects;
 
 // vertex shader filename
-std::string vShader = "../Shaders/vShader.txt";
+std::string vShader = SHADERS_DIR"/vShader.txt";
 // fragment shader filename
-std::string fShader = "../Shaders/fShader.txt";
+std::string fShader = SHADERS_DIR"/fShader.txt";
 
 void CreateObjects()
 {
@@ -33,21 +33,21 @@ void CreateObjects()
         0.0f, 2.0f, 0.0f,
         1.0f, 0.0f, 0.0f
     };
-    
+
     unsigned int elements[] = {
         0,3,1,
         1,3,2,
         2,3,0,
         0,1,2
     };
-    
+
     Mesh * obj1 = new Mesh();
     obj1->createMesh(vertices,elements,12,12);
     objects.push_back(obj1);
 }
 
 int main() {
-    
+
     // create & initialize main window
     Window mainWindow(WIDTH,HEIGHT);
     if (mainWindow.initialize() != 0)
@@ -55,72 +55,72 @@ int main() {
         std::cout << "Problem creating window!\n";
         return 1;
     }
-    
+
     // create objects on GPU
     CreateObjects();
-    
+
     // create camera object
     Camera camera(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,1.0f,0.0f),
                   -90.0f,0.0f,10.0f,0.05f);
-    
+
     // create and compile shaders on GPU
     Shader shader;
     shader.createFromFile(vShader, fShader);
     GLuint uniModel, uniView, uniProjection;
-    
+
     // projection matrix
     glm::mat4 projection(1.0f);
     projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth()/(GLfloat)mainWindow.getBufferHeight(), 0.1f, 100.0f);
-    
+
     // time tracking for delta t
     GLfloat lastTime = glfwGetTime();
-    
+
     // Loop until window closed
     while (!mainWindow.shouldClose())
     {
         // Get + Handle user input events
         glfwPollEvents();
-        
+
         // compute delta t
         GLfloat timeNow = glfwGetTime();
         GLfloat delta_t = timeNow - lastTime;
         lastTime = timeNow;
-        
+
         // pass data to the camera
         camera.keyControl(mainWindow.getKeys(),delta_t);
         camera.mouseControl(mainWindow.getChangeX(), mainWindow.getChangeY());
-        
+
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
         shader.useShader();
         uniModel = shader.getUniformModel();
         uniView = shader.getUniformView();
         uniProjection = shader.getUniformProjection();
         glUniformMatrix4fv(uniProjection,1,GL_FALSE,glm::value_ptr(projection));
         glUniformMatrix4fv(uniView,1,GL_FALSE,glm::value_ptr(camera.viewMatrix()));
-        
-        
+
+
         glm::mat4 model(1.0f);
         model = glm::translate(model, glm::vec3(0.0f,0.0f,-5.0f));
         //model = glm::scale(model,glm::vec3(0.5f,0.5f,1.0f));
         glUniformMatrix4fv(uniModel,1,GL_FALSE,glm::value_ptr(model));
         objects[0]->renderMesh();
-        
+
         model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(2.0f,2.0f,-5.0f));
         //model = glm::scale(model,glm::vec3(0.5f,0.5f,1.0f));
         glUniformMatrix4fv(uniModel,1,GL_FALSE,glm::value_ptr(model));
         objects[0]->renderMesh();
-        
+
         glUseProgram(0);
 
         mainWindow.swapBuffers();
     }
-    
+
     delete objects[0];
-    
-    
+
+
     return 0;
 }
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <cstring>
 
 Shader::Shader() :
     shaderID(0),
@@ -34,7 +35,7 @@ void Shader::deleteShader()
         glDeleteProgram(shaderID);
         shaderID = 0;
     }
-    
+
     uniModel = 0;
     uniProjection = 0;
 }
@@ -42,19 +43,19 @@ void Shader::deleteShader()
 void Shader::compileShader(const std::string &vShader, const std::string &fShader)
 {
     shaderID = glCreateProgram();
-    
+
     if (!shaderID)
     {
         std::cout << "Error creating shader program\n";
         return;
     }
-    
+
     addShader(vShader, GL_VERTEX_SHADER);
     addShader(fShader, GL_FRAGMENT_SHADER);
-    
+
     GLint result = 0;
     GLchar eLog[1024] = { 0 };
-    
+
     glLinkProgram(shaderID);
     glGetProgramiv(shaderID, GL_LINK_STATUS, &result);
     if (!result)
@@ -63,13 +64,13 @@ void Shader::compileShader(const std::string &vShader, const std::string &fShade
         std::cout << "Error linking shader program: " << eLog << std::endl;
         return;
     }
-    
+
     uniModel = glGetUniformLocation(shaderID,"model");
     uniView = glGetUniformLocation(shaderID,"view");
     uniProjection = glGetUniformLocation(shaderID,"projection");
-    
+
     //========== Validation ===//
-    
+
     glValidateProgram(shaderID);
     glGetProgramiv(shaderID, GL_VALIDATE_STATUS, &result);
     if (!result)
@@ -84,19 +85,19 @@ void Shader::compileShader(const std::string &vShader, const std::string &fShade
 void Shader::addShader(const std::string &shaderCode, GLenum shaderType)
 {
     GLuint shader = glCreateShader(shaderType);
-    
+
     const char* codes[1];
     codes[0] = shaderCode.c_str();
-    
+
     GLint codeLens[1];
     codeLens[0] = (GLint)strlen(codes[0]);
-    
+
     glShaderSource(shader, 1, codes, codeLens);
     glCompileShader(shader);
-    
+
     GLint result = 0;
     GLchar eLog[1024] = { 0 };
-    
+
     glGetShaderiv(shader,GL_COMPILE_STATUS,&result);
     if (!result)
     {
@@ -104,7 +105,7 @@ void Shader::addShader(const std::string &shaderCode, GLenum shaderType)
         std::cout << "Error compiling " << shaderType << " shader program: " << eLog << std::endl;
         return;
     }
-    
+
     glAttachShader(shaderID,shader);
 }
 
@@ -112,20 +113,20 @@ std::string Shader::readFile(const std::string &shaderCodeFilename)
 {
     std::string fileContent;
     std::ifstream fileStream(shaderCodeFilename,std::ios::in);
-    
+
     if (!fileStream.is_open())
     {
         std::cout << "File " << shaderCodeFilename << " does not exist\n";
         return "";
     }
-    
+
     std::string line = "";
     while (!fileStream.eof())
     {
         std::getline(fileStream,line);
         fileContent.append(line + "\n");
     }
-    
+
     fileStream.close();
     return fileContent;
 }
