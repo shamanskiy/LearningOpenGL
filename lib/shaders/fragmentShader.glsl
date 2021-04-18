e381593b6d4b3699f9620bee1d2b7f04c52f68e5
shamanskiy <shamanskiy.alexander@gmail.com>
2021-04-18 12:00:30 +0100

    refactor shader code

--- lib/shaders/fragmentShader.glsl
+++ lib/shaders/fragmentShader.glsl
@@ -1,8 +1,8 @@
 #version 330
 
-in vec2 texCoord;
+in vec2 posUV;
 in vec3 normal;
-in vec3 worldPosition;
+in vec3 pos3D;
 
 out vec4 colour;
 
@@ -20,6 +20,7 @@ struct Material
     vec3 diffuseColor;
 };
 
+
 uniform sampler2D texSampler;
 uniform Light light;
 uniform Material material;
@@ -27,23 +28,25 @@ uniform vec3 cameraPosition;
 
 void main()
 {
-    vec4 ambientColor = vec4(light.color * light.ambientIntensity, 1.0f);
-    vec4 matColor = vec4(material.diffuseColor, 1.0f);
-    // light direction is normalized at creation
-    float diffuseFactor = max(-1*dot(normalize(normal),light.direction),0.0f);
-    vec4 diffuseColor = vec4(light.color * light.diffuseIntensity * diffuseFactor, 1.0);
+    vec4 materialColor = vec4(material.diffuseColor, 1.0f);
+
+    // Phong lighting = ambient + diffuse + specular
+    vec4 phongColor = vec4(light.color * light.ambientIntensity, 1.0f);
+
+    // Compute diffuse light
+    // Light direction is normalized at creation
+    float incidentAngle = max(-1*dot(normalize(normal),light.direction),0.0f);
+    phongColor += vec4(light.color * light.diffuseIntensity * incidentAngle, 1.0);
     
-    vec4 specularColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
-    if (diffuseFactor > 0.0f)
+    // Compute specular light
+    if (incidentAngle > 0.0f)
     {
-        vec3 dirToCamera = normalize(cameraPosition - worldPosition);
+        vec3 dirToCamera = normalize(cameraPosition - pos3D);
         vec3 reflectedLight = reflect(light.direction,normalize(normal));
-        float specularFactor = dot(reflectedLight,dirToCamera);
-        if (specularFactor > 0.0f)
-        {
-            specularColor = vec4(light.color * pow(specularFactor,material.shininess), 1.0f);
-        }
+        float specularAngle = dot(reflectedLight,dirToCamera);
+        if (specularAngle > 0.0f)
+            phongColor += vec4(light.color * pow(specularAngle,material.shininess), 1.0f);
     }
     
-    colour = matColor * texture(texSampler, texCoord) * (ambientColor + diffuseColor + specularColor);
+    colour = materialColor * texture(texSampler, posUV) * phongColor;
 }
--- lib/shaders/vertexShader.glsl
+++ lib/shaders/vertexShader.glsl
@@ -4,9 +4,9 @@ layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 tex;
 layout (location = 2) in vec3 norm;
 
-out vec2 texCoord;
+out vec2 posUV;
 out vec3 normal;
-out vec3 worldPosition;
+out vec3 pos3D;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -16,9 +16,9 @@ void main()
 {
     gl_Position = projection * view * model * vec4(pos, 1.0);
 
-    texCoord = tex;
+    posUV = tex;
     
     normal = mat3(transpose(inverse(model)))*norm;
     
-    worldPosition = mat3(model) * pos;
+    pos3D = mat3(model) * pos;
 }
