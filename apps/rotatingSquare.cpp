2e9376d69b5502c68360dc37782c028f83550c0e
Shamanskiy <shamanskiy.alexander@gmail.com>
2022-02-06 21:20:10 +0000

    finish rotating square

--- apps/rotatingSquare.cpp
+++ apps/rotatingSquare.cpp
@@ -2,35 +2,46 @@
 #include "Shader.h"
 #include "Window.h"
 #include "Config.h"
-#include "Model.h"
+#include "Mesh.h"
 
 #include <vector>
 
-int main()
-{
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main() {
     Window window(600, 600, "Rotating Square");
 
     const ShaderProgram shader(SHADERS_DIR + "rotatingSquareVertex.glsl", SHADERS_DIR + "rotatingSquareFragment.glsl");
     shader.activateShader();
 
-    const std::vector<GLfloat> vertices{
-        -0.5f, -0.5f, 0.0f, // vertex 1
-         0.5f, -0.5f, 0.0f, // vertex 2
-        -0.5f,  0.5f, 0.0f, // vertex 3
-         0.5f,  0.5f, 0.0f, // vertex 4
+    const std::vector<GLfloat> vertices {
+        -0.7f, -0.7f, 0.0f, // vertex 1
+         0.7f, -0.7f, 0.0f, // vertex 2
+        -0.7f,  0.7f, 0.0f, // vertex 3
+         0.7f,  0.7f, 0.0f, // vertex 4
     };
-    const std::vector<GLuint> indices{
+    const std::vector<GLuint> indices {
         0, 1, 2, // triangle 1
         1, 2, 3  // triangle 2
     };
-    const Mesh triangle(vertices, indices, VertexData::POSITION);
-    
-    while (!window.shouldClose())
-    {
+    const Mesh square(vertices, indices, VertexData::POSITION);
+
+    // Get location index of the "rotation" variable in the shader
+    GLint rotationLocation = glGetUniformLocation(shader.id(), "rotation");
+    glm::mat4 rotation = glm::mat4(1.0f);
+        
+    while (!window.shouldClose()) {
         window.pollEvents();
 
+        // increment the rotation at every iteration
+        rotation = glm::rotate(rotation, glm::radians(0.5f), glm::vec3(0.0f, 0.0f, 1.0f));
+        // send the rotation matrix to the GPU using the known location
+        glUniformMatrix4fv(rotationLocation, 1, GL_FALSE, glm::value_ptr(rotation));
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        triangle.render();   
+        square.render();   
         
         window.swapBuffers();
     }
--- lib/shaders/rotatingSquareVertex.glsl
+++ lib/shaders/rotatingSquareVertex.glsl
@@ -5,10 +5,13 @@ layout(location = 0) in vec3 vertexCoordinates;
 // Output vector that will be passed to the fragment shader.
 out vec3 position3D;
 
+uniform mat4 rotation;
+
 void main() {
     // Built-in vector of homogeneous vertex coordinates.
     // Has to be assigned before rasterization.
-    gl_Position = vec4(vertexCoordinates, 1.0);
+    gl_Position = rotation * vec4(vertexCoordinates, 1.0);
     // Pass the coordinates to the fragment shader without change.
+    //position3D = gl_Position.xyz;
     position3D = vertexCoordinates;
 }
